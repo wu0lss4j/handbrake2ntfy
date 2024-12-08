73573f0def4609e812d3cb1aeceaf74710a36eec
Bruno <147919252+wu0lss4j@users.noreply.github.com>
2024-12-08 19:16:14 +0100

    Update handbrake2ntfy.ps1

--- handbrake2ntfy.ps1
+++ handbrake2ntfy.ps1
@@ -2,10 +2,19 @@
 ## Handbrake 2 ntfy.sh
 ## by Bruno (wu0lss4j@duck.com)
 ##
+## 2024.12.08 barely an update
+##
 ## Short PowerShell Core 7 script to send a notification using ntfy when your Handbrake encode is finished
+## inspired by this post → https://github.com/HandBrake/HandBrake/discussions/5617
 ##
 ## Handbrake -- https://github.com/HandBrake
 ## ntfy.sh -- https://github.com/binwiederhier/ntfy
+##
+## BASIC SETUP
+## 1. Drop this .ps1 script on your path
+## 2. Go to Tools > Preferences > When Done > enable Send file to: > Browse to pwsh.exe
+## 3. Enter this in arguments: handbrake2ntfy.ps1 {destination} encode
+## Note: make sure you have ntfy listening to the word "encode"
 
 ## TODO
 ## add arg%3 for allow the specification of a custom ntfy server
